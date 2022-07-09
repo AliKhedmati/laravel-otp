ed2e786e3f97b039377e99258fd7c312f2b57bf0
Ali Khedmati <ali@khedmati.ir>
2022-07-10 02:13:44 +0430

    Debug.

--- src/OtpServiceProvider.php
+++ src/OtpServiceProvider.php
@@ -17,17 +17,17 @@ class OtpServiceProvider extends ServiceProvider
     protected function offerPublishing()
     {
         $this->publishes([
-            __DIR__ .'config/otp.php'   =>   config_path('otp.php')
+            __DIR__ . '/config/otp.php'   =>   config_path('otp.php')
         ], 'config');
 
         $this->publishes([
-            __DIR__ . 'lang'    =>  $this->app->langPath('vendor/otp')
+            __DIR__ . '/lang'    =>  $this->app->langPath('vendor/otp')
         ]);
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . 'config/otp.php', 'otp');
+        $this->mergeConfigFrom(__DIR__ . '/config/otp.php', 'otp');
 
         $this->app->bind(OtpInterface::class, fn() => new Otp());
     }
