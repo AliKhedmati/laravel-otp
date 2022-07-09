44701b10704e09c517716ce4a581bcd10094e0c7
Ali Khedmati <ali@khedmati.ir>
2022-07-10 03:43:28 +0430

    Langs restructured.

--- src/lang/en/otp.php
+++ src/lang/en/otp.php
@@ -1,12 +1,10 @@
 <?php
 
 return [
-    'otp'   =>  [
-        'sent'  =>  'OTP has been sent successfully.',
-        'wait'  =>  'Please wait :seconds more seconds.',
-        'failed'    =>  'Authorization has failed.',
-        'expired'   =>  'OTP has expired.',
-        'not-valid' =>  'OTP is not valid.',
-        'successful'    =>  'Authorization has completed successfully.',
-    ],
+    'sent'  =>  'OTP has been sent successfully.',
+    'wait'  =>  'Please wait :seconds more seconds.',
+    'failed'    =>  'Authorization has failed.',
+    'expired'   =>  'OTP has expired.',
+    'not-valid' =>  'OTP is not valid.',
+    'successful'    =>  'Authorization has completed successfully.',
 ];
